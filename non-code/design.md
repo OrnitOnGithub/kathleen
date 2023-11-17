bedb1f8d13c7d6fec49a6d8decc124d6fea5bc8c
ornit <mtedesco@Etu7310.local>
2023-11-17 11:15:08 +0100

    yapped

--- non-code/design.md
+++ non-code/design.md
@@ -35,87 +35,12 @@ mov rdi 0
 syscall
 ```
 
-# Memory allocation
+## Main function
 
-Static variables will be preallocated.
-
-Non-static variables can have their pointers preallocated.
-
-Dynamically allocated variables (lists) are yet to be figured out.
-
-<br>
-
-Pointers can also be preallocated
-
-The stack can be used.
-
-Rust for example stores the pointers to the heap in the stack.
-
-Holy mother of god the stack pointer seems to be manipulatable
-```asm
-; Some code that pushes values onto the stack
-
-    ; Move the stack pointer to access the 11th value
-    mov rax, 10          ; Index of the 11th value (zero-based)
-    mov rbx, 8           ; Size of each value (assuming 64-bit architecture)
-    imul rax, rbx        ; Calculate the offset
-    sub rsp, rax         ; Adjust the stack pointer
-
-    ; Now you can access the value at [rsp]
-
-    ; Example: print the value at [rsp]
-    mov rdi, 1            ; file descriptor (stdout)
-    mov rsi, rsp          ; pointer to the value
-    mov rdx, 8            ; size of the value (assuming 64-bit)
-    mov rax, 1            ; sys_write system call number
-    syscall
-
-    ; Restore the stack pointer to its original position
-    add rsp, rax
-```
-
-## Let
-
-Language
-
-```
-let variable = text
-```
-
-Assembly
-
-```asm
-section .data
-	text db "Hello", 0          
-
-section .bss
-    variable resq 1     ; Reserve space for a 64-bit variable
-
-_start:
-    ; (in the language) let is used
-    ; Initialize the variable with a value
-
-    mov qword [variable], text  ; qword = 8 bytes, word = 2 bytes for example.
-                                ; To store 2 words for example, we would
-                                ; need to use [variable + x]
-```
-
-## Drop
-
-Language
-
-```
-drop variable
-```
-
-Assembly
-
-```asm
-; (in the language) drop is used
-; manually deallocate the memory
-
-mov rax, 11             ; syscall number for sys_munmap (11)
-mov rdi, [variable]     ; Pointer to the variable
-mov rsi, 6 
-syscall
+```c
+main(parameter: int) {
+    // this program prints whatever parameter 
+    // it is given when ran in the coneolse
+    print(parameter)
+}
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use std::fs::read_to_string;
 
-const FILEPATH: &str = "src/mylang.c"; // Later the compiler should take this as a parameter.
+// Later the compiler should take this as a parameter.
+const FILEPATH: &str = "src/mylang.c";
 
 // From now on in comments, "the code" refers to the
 // programming language this compiler compiles for.
 
 fn main() {
 
-    // Create a Vector for each line of the code.
-    //
-    // The only reason we separate lines is for the 
-    // compiler to be able to return a line number when
-    // an error occurs. All we do is return the index + 1.
+    /// Create a Vector for each line of the code.
+    ///
+    /// The only reason we separate lines is for the 
+    /// compiler to be able to return a line number when
+    /// an error occurs. All we do is return the index + 1.
     let mut lines = Vec::new();
     // Iterate through the lines, add to Vector
     for line in read_to_string(FILEPATH).unwrap().lines() {
@@ -19,12 +20,13 @@ fn main() {
     }
 
 
-    // Tokenise the code by splitting every whitespace character.
-    // Store all tokens in a vector
-    // We get a Vector like this:
-    // 
-    //   line 1         line 2
-    // [["Hello"], ["hi", "there"]]
+    /// Tokenise the code by splitting every whitespace character.
+    /// Store all tokens in a vector
+    /// We get a Vector like this:
+    /// ```
+    ///   line 1         line 2
+    /// [["Hello"], ["hi", "there"]]
+    /// ```
     let mut tokenised_lines = Vec::new();
     for line in lines {
         // Create a new vector with every token
@@ -33,19 +35,27 @@ fn main() {
     println!("tokens: {:?}", tokenised_lines);
 
 
-    
+    //
 
 
 }
 
-
+/// Represents an instruction or a set of instructions
+/// in the intermediate representation.
+///
+/// `inst_type` defines the type of instruction, while `parameters`
+/// contains a vector of additional instructions or arguments
+/// associated with this instruction.
 //#[derive(Debug)]
 struct Instruction {
-    instruction_type: Type,
-    parameters: Vec<Instruction>,    
+    inst_type: Type,
+    parameters: Vec<Instruction>,  
 }
 
-
+/// This Enum enumerates different types of "instructions",
+/// including definitions for sections like `.data`, loop
+/// constructs, conditions, identifiers,
+/// constants, and various data types.
 //#[derive(Debug)]
 enum Type {
     StaticData,         // Section .data
@@ -64,6 +74,8 @@ enum Type {
     Identifier(i32),    // To differentiate between for example jump points.
                         // Must be incremented after each use
     List,
+    Const,              // Lifetime = whole execution, immutable
+    Static,             // Lifetime = whole execution, it is mutable though
     Int(i32),
     Float(f32),
     String(String),
