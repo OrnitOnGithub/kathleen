2014a777c7e8289b5d6c2d62aa47f8df8697a887
lea <sex@sex.com>
2024-02-26 21:34:53 +0100

    saveing changes so i can delete .vscode from github repo

--- src/asm_generator.rs
+++ src/asm_generator.rs
@@ -2,7 +2,11 @@ use crate::nar_generator::{NAI,NAR};
 
 pub fn generate_asm(nar: NAR) -> String {
 
+    let asm: String = String::new();
 
+    for x in nar.data {
+        
+    }
 
-    return String::from("testing");
+    return asm;
 }
