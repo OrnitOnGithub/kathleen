15f425183d39335d41f1c602121edc02db269b7d
lea <sex@sex.com>
2024-02-27 21:08:06 +0100

    asdf

--- src/asm_generator.rs
+++ src/asm_generator.rs
@@ -1,11 +1,21 @@
 use crate::nar_generator::{NAI,NAR};
 
+const asm_path: &str = "/asm/nasm-x64";
+
 pub fn generate_asm(nar: NAR) -> String {
 
-    let asm: String = String::new();
+    let mut asm: String = String::new();
 
     for x in nar.data {
-        
+        asm += &generate_asm_block(x);
+    }
+
+    fn generate_asm_block(nai: NAI) -> String {
+        let asm: String = String::new();
+
+
+
+        return asm;
     }
 
     return asm;
