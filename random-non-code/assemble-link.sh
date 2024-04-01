b19f3d3450b7ace15df26ffd15ff0144136ddf62
ornit <ornithopterthethopter@gmail.com>
2024-04-02 00:09:44 +0200

    Renamed the project to kathleen

--- random-non-code/assemble-link.sh
+++
@@ -1,3 +0,0 @@
-nasm -f elf64 output.asm -o output.o -g
-
-gcc -no-pie output.o -o output -g
--- random-non-code/runoutput.sh
+++
@@ -1,5 +0,0 @@
-nasm -f elf64 output.asm -o output.o -g
-
-gcc -no-pie output.o -o output -g
-
-./output
