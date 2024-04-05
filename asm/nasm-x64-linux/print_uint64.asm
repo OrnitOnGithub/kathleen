8227336c33900e4dc43f990efd3241657ee81b34
ornit <ornithopterthethopter@gmail.com>
2024-04-05 22:36:51 +0200

    Added a comment to print_uint64.asm

--- asm/nasm-x64-linux/print_uint64.asm
+++ asm/nasm-x64-linux/print_uint64.asm
@@ -4,7 +4,7 @@ mov rbx, [rax]
 mov rax, [rbx]  ; Get the value by double-dereference
 mov     rdi, uint64_as_str_buffer + 38
 call uint64_to_str ; convert int to string, put in uint64_as_str_buffer
-; print uint64_as_str_buffer, which now contains our int as strint.
+; print uint64_as_str_buffer, which now contains our int as string.
 mov         rdx,    39
 mov         rsi,    uint64_as_str_buffer
 mov         rdi,    1
