7d9de1d83190bc9b3c4d5ed4c4f5e72f8d5c8730
ornit <ornithopterthethopter@gmail.com>
2024-04-17 23:01:09 +0200

    Updated to recieve the Parameters struct and other minor changes

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use std::time::Instant;
 // Colours in the terminal
 use colored::*;
 
+/// Module for interacting with the user. Mainly handles
+/// command line arguments.
+mod cli;
+
 /// This is the code for the tokenizer; first step of compilation.
 /// Check `src/tokenizer.rs` for more info.
 mod tokenizer;
@@ -28,23 +32,13 @@ mod asm_generator;
 /// Check `src/error.rs` for more info.
 mod error;
 
-/// Module for interacting with the user. Mainly handles
-/// command line arguments.
-mod cli;
-
 // From now on in comments, "the code" refers to the
 // programming language this compiler compiles for.
 fn main() {
   // get info from the command line arguments
-  let (
-    source_file_path, // path to source code
-    output_file_path, // name of output
-    dont_assemble, // if true, stop compilation before assembly code gets assembled
-    dont_link, // if true, stop compilation before object file gets linked
-    keep_intermediate_files, // if true, don't delete the assembly and object files (intermediate files) after compilation
-  ) = cli::handle_args();
+  let parameters: cli::Parameters = cli::handle_args();
 
-  println!("{} {}\n", "Kathleen: compiling".green(), source_file_path.green().italic());
+  println!("{} {}\n", "Kathleen: compiling".green(), parameters.file_path.green().italic());
 
   // For funsies, let's count how long compilation takes.
   let compilation_start_time = Instant::now();
@@ -54,7 +48,7 @@ fn main() {
   // each token is in.
   let mut code_lines: Vec<String> = Vec::new();
   // Iterate through the lines, add to Vector
-  for line in fs::read_to_string(source_file_path).unwrap().lines() {
+  for line in fs::read_to_string(parameters.file_path).unwrap().lines() {
     code_lines.push(line.to_string());
   }
 
@@ -69,16 +63,18 @@ fn main() {
     = ir_generator::generate_ir(tokens);
 
   // Generate the near assembly representation using the intermediate representation.
-  let near_assembly_representation 
+  let near_assembly_representation
     = nar_generator::generate_nar(intermediate_representation);
 
   // Generate the assembly output using the NAR
   let assembly_output
     = asm_generator::generate_asm(near_assembly_representation);
 
+  // The following could totally be done in a different module. Not sure if useful.
+
   // Define file names for the intermediate files.
-  let asm_path = &format!("{}.asm", output_file_path);
-  let obj_path = &format!("{}.o", output_file_path);
+  let asm_path = &format!("{}.asm", parameters.output_path);
+  let obj_path = &format!("{}.o", parameters.output_path);
   // Then write the assembly output to a file
   let _file = fs::File::create(asm_path);
   fs::write(asm_path, assembly_output)
@@ -89,7 +85,7 @@ fn main() {
 
   // Assemble and link the assembly output file
   let mut issue_during_assembling_or_linking: bool = false;
-  if !dont_assemble {
+  if !parameters.dont_assemble {
     // run the command `nasm -f elf64 output.asm -o output.o -g`
     let mut assemble = process::Command::new("nasm");
     assemble.arg("-f").arg("elf64").arg(asm_path).arg("-o").arg(obj_path).arg("-g");
@@ -104,10 +100,10 @@ fn main() {
       issue_during_assembling_or_linking = true;
     }
   }
-  if !dont_link && !dont_assemble{
+  if !parameters.dont_link && !parameters.dont_assemble{
     // run the command `gcc -no-pie output.o -o output -g`
     let mut link = process::Command::new("gcc");
-    link.arg("-no-pie").arg(obj_path).arg("-o").arg(output_file_path.clone()).arg("-g");
+    link.arg("-no-pie").arg(obj_path).arg("-o").arg(parameters.output_path.clone()).arg("-g");
     let link_output = link.output()
       .expect("Failed to run command. Is GCC installed?");
     println!("{}: [{}]",
@@ -120,17 +116,24 @@ fn main() {
     }
   }
 
-  if !issue_during_assembling_or_linking && !dont_assemble && !dont_link {
+  if !issue_during_assembling_or_linking &&
+     !parameters.dont_assemble           &&
+     !parameters.dont_link
+  {
     let compilation_duration = compilation_start_time.elapsed();
     println!("{}{:?}{}{}",
-    "Compilation successful in ".green(),
-    compilation_duration,
-    "! Run the output binary with ./".green(),
-    output_file_path.green().italic()
+      "Compilation successful in ".green(),
+      compilation_duration,
+      "! Run the output binary with ./".green(),
+      parameters.output_path.green().italic()
     );
   }
 
-  if !keep_intermediate_files && !dont_assemble && !dont_link && !issue_during_assembling_or_linking {
+  if !parameters.keep_intermediate_files &&
+     !parameters.dont_assemble           &&
+     !parameters.dont_link               &&
+     !issue_during_assembling_or_linking
+  {
     fs::remove_file(asm_path).expect("DEV: Couldn't remove asm path");
     fs::remove_file(obj_path).expect("DEV: Couldn't remove out path");
   }
